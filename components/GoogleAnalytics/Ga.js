9d0bf362f1ab713a33bbdb6a3603280f33c8b5dc
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-09-25 03:17:39 +0530

    Correcting the mis-type in the google analytics ID

--- components/GoogleAnalytics/Ga.js
+++ components/GoogleAnalytics/Ga.js
@@ -3,14 +3,14 @@ import Script from "next/script";
 function Ga() {
   return (
     <div className="container">
-      <Script src="https://www.googletagmanager.com/gtag/js?id=7Q6P5VX977" />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-7Q6P5VX977" />
       <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
  
-          gtag('config', '7Q6P5VX977');
+          gtag('config', 'G-7Q6P5VX977');
         `}
       </Script>
     </div>
